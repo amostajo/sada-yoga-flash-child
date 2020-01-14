e7d08f08f9c4aaf5bf5f4cffa6ea78b131507e3c
Alejandro Mostajo <amostajo@gmail.com>
2020-01-14 04:33:33 -0600

    Update ConfigController.php
    Fixes polylang check.

--- app/Controllers/ConfigController.php
+++ app/Controllers/ConfigController.php
@@ -65,9 +65,11 @@ class ConfigController extends Controller
      */
     public function asset_url_or_path($url_or_path)
     {
-        $lang = pll_current_language('slug');
-        if (strpos($url_or_path, '/' . $lang . '/') !== false) {
-            $url_or_path = str_replace('/' . $lang, '', $url_or_path);
+        if (function_exists('pll_current_language')) {
+            $lang = pll_current_language('slug');
+            if (strpos($url_or_path, '/' . $lang . '/') !== false) {
+                $url_or_path = str_replace('/' . $lang, '', $url_or_path);
+            }
         }
         return $url_or_path;
     }
